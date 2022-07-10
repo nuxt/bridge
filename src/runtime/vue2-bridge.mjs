6c209ff81e83eb3028a30ac31018982fdb0c01bd
Daniel Roe <daniel@roe.dev>
2022-07-10 10:42:02 +0100

    fix: restore vue2-compat alias and de-default export

--- src/runtime/vue2-bridge.mjs
+++ src/runtime/vue2-bridge.mjs
@@ -1,3 +1,15 @@
+import Vue from 'vue'
 export * from 'vue'
 
 export const isFunction = fn => fn instanceof Function
+
+// @ts-ignore
+const defaultVue = Vue.default || Vue
+
+export { defaultVue as default }
+
+// mock for vue-demi
+export const Vue2 = defaultVue
+export const isVue2 = true
+export const isVue3 = false
+export const install = () => {}
--- src/vue-compat.ts
+++ src/vue-compat.ts
@@ -1,8 +1,9 @@
 import { pathToFileURL } from 'url'
 import MagicString from 'magic-string'
-import { findStaticImports } from 'mlly'
+import { findExports, findStaticImports } from 'mlly'
 import { parseQuery, parseURL } from 'ufo'
 import { createUnplugin } from 'unplugin'
+import escapeRE from 'escape-string-regexp'
 
 export const VueCompat = createUnplugin((opts: { src?: string }) => {
   return {
@@ -28,10 +29,15 @@ export const VueCompat = createUnplugin((opts: { src?: string }) => {
       if (id.includes('vue2-bridge')) { return }
 
       const s = new MagicString(code)
-      const imports = findStaticImports(code).filter(i => i.type === 'static' && vueAliases.includes(i.specifier))
+      const references = [
+        ...findStaticImports(code).filter(i => i.type === 'static' && vueAliases.includes(i.specifier)),
+        ...findExports(code).filter(i => vueAliases.includes(i.specifier))
+      ]
 
-      for (const i of imports) {
-        s.overwrite(i.start, i.end, i.code.replace(`"${i.specifier}"`, `"${opts.src}"`).replace(`'${i.specifier}'`, `'${opts.src}'`))
+      for (const i of references) {
+        const escapedSpecifier = escapeRE(i.specifier)
+        const specifierRE = new RegExp(`"${escapedSpecifier}"|'${escapedSpecifier}'`, 'g')
+        s.overwrite(i.start, i.end, i.code.replace(specifierRE, r => r.replace(i.specifier, opts.src)))
       }
 
       if (s.hasChanged()) {
@@ -45,9 +51,30 @@ export const VueCompat = createUnplugin((opts: { src?: string }) => {
 })
 
 const vueAliases = [
+  // vue
+  'vue',
   // vue 3 helper packages
   '@vue/shared',
   '@vue/reactivity',
   '@vue/runtime-core',
-  '@vue/runtime-dom'
+  '@vue/runtime-dom',
+  // vue-demi
+  'vue-demi',
+  ...[
+    // vue 2 dist files
+    'vue/dist/vue.common.dev',
+    'vue/dist/vue.common',
+    'vue/dist/vue.common.prod',
+    'vue/dist/vue.esm.browser',
+    'vue/dist/vue.esm.browser.min',
+    'vue/dist/vue.esm',
+    'vue/dist/vue',
+    'vue/dist/vue.min',
+    'vue/dist/vue.runtime.common.dev',
+    'vue/dist/vue.runtime.common',
+    'vue/dist/vue.runtime.common.prod',
+    'vue/dist/vue.runtime.esm',
+    'vue/dist/vue.runtime',
+    'vue/dist/vue.runtime.min'
+  ].flatMap(m => [m, `${m}.js`])
 ]
