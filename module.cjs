9be3a447642312eea2bbf1a2046a7c674147c7fc
Daniel Roe <daniel@roe.dev>
2022-07-12 10:20:21 +0100

    fix: set nuxt context and normalise config as first module (#397)

--- module.cjs
+++ module.cjs
@@ -6,21 +6,42 @@ module.exports = function (...args) {
 const pkg = require('./package.json')
 
 module.exports.defineNuxtConfig = (config = {}) => {
-  if (config.bridge !== false) {
-    // Add new handlers options
-    config.serverHandlers = config.serverHandlers || []
-    config.devServerHandlers = config.devServerHandlers || []
+  // Nuxt kit depends on this flag to check bridge compatibility
+  config.bridge = config.bridge ?? true
 
-    config.bridge = config.bridge || {}
-    config.bridge._version = pkg.version
-    if (!config.buildModules) {
-      config.buildModules = []
-    }
-    if (!config.buildModules.find(m => m === '@nuxt/bridge' || m === '@nuxt/bridge-edge')) {
-      // Ensure other modules register their hooks before
-      config.buildModules.push('@nuxt/bridge')
-    }
+  if (!config.bridge) { return config }
+
+  // Add new handlers options
+  config.serverHandlers = config.serverHandlers || []
+  config.devServerHandlers = config.devServerHandlers || []
+
+  config.bridge = config.bridge || {}
+  config.bridge._version = pkg.version
+  if (!config.buildModules) {
+    config.buildModules = []
   }
+  if (!config.buildModules.find(m => m === '@nuxt/bridge' || m === '@nuxt/bridge-edge')) {
+    // Ensure other modules register their hooks before
+    config.buildModules.push('@nuxt/bridge')
+  }
+  config.buildModules.unshift(async function () {
+    const nuxt = this.nuxt
+
+    const { nuxtCtx } = await import('@nuxt/kit')
+
+    // Allow using kit composables in all modules
+    if (nuxtCtx.use()) {
+      nuxtCtx.unset()
+    }
+    nuxtCtx.set(nuxt)
+
+    // Mock _layers for nitro and auto-imports
+    nuxt.options._layers = nuxt.options._layers || [{
+      config: nuxt.options,
+      cwd: nuxt.options.rootDir,
+      configFile: nuxt.options._nuxtConfigFile
+    }]
+  })
   return config
 }
 
