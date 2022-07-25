653fa9241f3b979aa896d8e014a21b1b7a8ab455
panyaohao@cmcm.com <panyaohao@cmcm.com>
2022-07-25 17:07:14 +0800

    fix: ie don't support allow function

--- src/vite/manifest.ts
+++ src/vite/manifest.ts
@@ -65,7 +65,7 @@ export async function generateBuildManifest (ctx: ViteBuildContext) {
     'var appConfig = window && window.__NUXT__ && window.__NUXT__.config.app || {}',
     'var publicBase = appConfig.cdnURL || appConfig.baseURL || "/"',
     `var imports = ${JSON.stringify(clientImports)};`,
-    'imports.reduce((p, id) => p.then(() => System.import(publicBase + appConfig.buildAssetsDir.slice(1) + id)), Promise.resolve())'
+    'imports.reduce(function(p, id){return p.then(function(){return System.import(publicBase + appConfig.buildAssetsDir.slice(1) + id)})}, Promise.resolve())'
   ].join('\n')
   const clientEntryName = 'entry-legacy.' + hash(clientEntryCode) + '.js'
 
