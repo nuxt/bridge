77239a0bccb7554a02bdef2c0c0f1332c3568c5d
Daniel Roe <daniel@roe.dev>
2022-10-22 14:29:05 +0100

    chore: disable lint rule

--- playground/pages/error.vue
+++ playground/pages/error.vue
@@ -7,5 +7,6 @@
 
 <script setup>
 throw createError({ statusCode: 422, fatal: true, statusMessage: 'This is a custom error' })
+// eslint-disable-next-line no-unreachable
 const state = ref({ attr: 'Hello World' })
 </script>
