c2cd154276c29fcd69643750b5a0530f08975619
Daniel Roe <daniel@roe.dev>
2023-04-24 20:47:28 +0100

    chore: add `@nuxt/types` to `peerDependencies`

--- packages/bridge/build.config.ts
+++ packages/bridge/build.config.ts
@@ -7,6 +7,7 @@ export default defineBuildConfig({
     { input: 'src/runtime/', outDir: 'dist/runtime', format: 'esm', declaration: true }
   ],
   externals: [
+    '@nuxt/types',
     'webpack',
     'vite',
     'vue',
