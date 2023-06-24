a12932aacdff86aa8393f34f5103e253eece1a88
M-Talhaouy <tal7aouy.dev@gmail.com>
2023-06-24 13:10:53 +0100

    improved

--- scripts/bump-edge.ts
+++ scripts/bump-edge.ts
@@ -3,24 +3,32 @@ import { execSync } from 'child_process'
 import { resolve } from 'pathe'
 import { globby } from 'globby'
 
-// Temporary forked from nuxt/framework
-
 interface Dep {
-  name: string,
-  range: string,
+  name: string
+  range: string
   type: string
 }
 
-async function loadPackage (dir: string) {
+async function loadPackage(dir: string) {
   const pkgPath = resolve(dir, 'package.json')
-  const data = JSON.parse(await fsp.readFile(pkgPath, 'utf-8').catch(() => '{}'))
-  const save = () => fsp.writeFile(pkgPath, JSON.stringify(data, null, 2) + '\n')
+  const data = JSON.parse(
+    await fsp.readFile(pkgPath, 'utf-8').catch(() => '{}')
+  )
+  const save = () =>
+    fsp.writeFile(pkgPath, JSON.stringify(data, null, 2) + '\n')
 
   const updateDeps = (reviver: (dep: Dep) => Dep | void) => {
-    for (const type of ['dependencies', 'devDependencies', 'optionalDependencies', 'peerDependencies']) {
-      if (!data[type]) { continue }
-      for (const e of Object.entries(data[type])) {
-        const dep: Dep = { name: e[0], range: e[1] as string, type }
+    for (const type of [
+      'dependencies',
+      'devDependencies',
+      'optionalDependencies',
+      'peerDependencies',
+    ]) {
+      if (!data[type]) {
+        continue
+      }
+      for (const [name, range] of Object.entries(data[type])) {
+        const dep: Dep = { name, range: range as string, type }
         delete data[type][dep.name]
         const updated = reviver(dep) || dep
         data[updated.type] = data[updated.type] || {}
@@ -33,27 +41,31 @@ async function loadPackage (dir: string) {
     dir,
     data,
     save,
-    updateDeps
+    updateDeps,
   }
 }
 
 type ThenArg<T> = T extends PromiseLike<infer U> ? U : T
 type Package = ThenArg<ReturnType<typeof loadPackage>>
 
-async function loadWorkspace (dir: string) {
+async function loadWorkspace(dir: string) {
   const workspacePkg = await loadPackage(dir)
-  const pkgDirs = (await globby(['packages/*'], { onlyDirectories: true })).sort()
+  const pkgDirs = (
+    await globby(['packages/*'], { onlyDirectories: true })
+  ).sort()
 
   const packages: Package[] = []
 
   for (const pkgDir of pkgDirs) {
     const pkg = await loadPackage(pkgDir)
-    if (!pkg.data.name) { continue }
+    if (!pkg.data.name) {
+      continue
+    }
     packages.push(pkg)
   }
 
   const find = (name: string) => {
-    const pkg = packages.find(pkg => pkg.data.name === name)
+    const pkg = packages.find((pkg) => pkg.data.name === name)
     if (!pkg) {
       throw new Error('Workspace package not found: ' + name)
     }
@@ -61,8 +73,9 @@ async function loadWorkspace (dir: string) {
   }
 
   const rename = (from: string, to: string) => {
-    find(from).data._name = find(from).data.name
-    find(from).data.name = to
+    const fromPkg = find(from)
+    fromPkg.data._name = fromPkg.data.name
+    fromPkg.data.name = to
     for (const pkg of packages) {
       pkg.updateDeps((dep) => {
         if (dep.name === from && !dep.range.startsWith('npm:')) {
@@ -83,7 +96,7 @@ async function loadWorkspace (dir: string) {
     }
   }
 
-  const save = () => Promise.all(packages.map(pkg => pkg.save()))
+  const save = () => Promise.all(packages.map((pkg) => pkg.save()))
 
   return {
     dir,
@@ -92,17 +105,17 @@ async function loadWorkspace (dir: string) {
     save,
     find,
     rename,
-    setVersion
+    setVersion,
   }
 }
 
-async function main () {
+async function main() {
   const workspace = await loadWorkspace(process.cwd())
 
   const commit = execSync('git rev-parse --short HEAD').toString('utf-8').trim()
   const date = Math.round(Date.now() / (1000 * 60))
 
-  for (const pkg of workspace.packages.filter(p => !p.data.private)) {
+  for (const pkg of workspace.packages.filter((p) => !p.data.private)) {
     workspace.setVersion(pkg.data.name, `${pkg.data.version}-${date}.${commit}`)
     workspace.rename(pkg.data.name, pkg.data.name + '-edge')
   }
