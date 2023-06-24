48df8e89dc3be07c1f0042d8d09eb36b4dd6b198
wattanx <wattan.dev@gmail.com>
2023-06-24 12:23:31 +0900

    feat: add baseURL types

--- packages/bridge-schema/src/config/app.ts
+++ packages/bridge-schema/src/config/app.ts
@@ -23,6 +23,18 @@ export default defineUntypedSchema({
 
   app: {
     /**
+     * The base path of your Nuxt application.
+     *
+     * This can be set at runtime by setting the NUXT_APP_BASE_URL environment variable.
+     * @example
+     * ```bash
+     * NUXT_APP_BASE_URL=/prefix/ node .output/server/index.mjs
+     * ```
+     */
+    baseURL: {
+      $resolve: val => val || process.env.NUXT_APP_BASE_URL || '/'
+    },
+    /**
      * The folder name for the built site assets, relative to `baseURL` (or `cdnURL` if set).
      * @deprecated - use `buildAssetsDir` instead
      */
