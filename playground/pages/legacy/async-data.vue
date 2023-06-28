41c24084f9a8506ac33a94c8b1af0d1034db0c4d
wattanx <wattan.dev@gmail.com>
2023-06-28 23:02:18 +0900

    feat: add legacy asyncData test

---
+++ playground/pages/legacy/async-data.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>{{ hello }}</div>
+  </div>
+</template>
+
+<script>
+export default defineNuxtComponent({
+  fetchKey: () => 'hello',
+  async asyncData () {
+    await nextTick()
+    return {
+      hello: await $fetch('/api/hello')
+    }
+  }
+})
+</script>
