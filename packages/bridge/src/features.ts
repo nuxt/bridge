94246e6e2ab1e18eecc3058dee9d3da93a372376
wattanx <wattan.dev@gmail.com>
2023-09-13 09:25:47 +0900

    lint fix

--- packages/bridge/src/features.ts
+++ packages/bridge/src/features.ts
@@ -1,44 +1,44 @@
-import { addDependency } from "nypm";
-import { resolvePackageJSON } from "pkg-types";
-import { logger } from "@nuxt/kit";
-import { isCI } from "std-env";
-import prompts from "prompts";
+import { addDependency } from 'nypm'
+import { resolvePackageJSON } from 'pkg-types'
+import { logger } from '@nuxt/kit'
+import { isCI } from 'std-env'
+import prompts from 'prompts'
 
-export async function ensurePackageInstalled(
+export async function ensurePackageInstalled (
   rootDir: string,
   name: string,
   searchPaths?: string[]
 ) {
   if (await resolvePackageJSON(name, { url: searchPaths }).catch(() => null)) {
-    return true;
+    return true
   }
 
-  logger.info(`Package ${name} is missing`);
+  logger.info(`Package ${name} is missing`)
   if (isCI) {
-    return false;
+    return false
   }
 
   const { confirm } = await prompts({
-    type: "confirm",
-    name: "confirm",
+    type: 'confirm',
+    name: 'confirm',
     message: `Do you want to install ${name} package?`,
-    initial: true,
-  });
+    initial: true
+  })
 
   if (!confirm) {
-    return false;
+    return false
   }
 
-  logger.info(`Installing ${name}...`);
+  logger.info(`Installing ${name}...`)
   try {
     await addDependency(name, {
       cwd: rootDir,
-      dev: true,
-    });
-    logger.success(`Installed ${name}`);
-    return true;
+      dev: true
+    })
+    logger.success(`Installed ${name}`)
+    return true
   } catch (err) {
-    logger.error(err);
-    return false;
+    logger.error(err)
+    return false
   }
 }
