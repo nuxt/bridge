5c87c30b122d8b5a4d6121d7b6ebfce4ffd06a1e
Ryota Watanabe <43837308+wattanx@users.noreply.github.com>
2023-10-30 21:53:07 +0900

    fix: avoid using shared global state for head (#916)
    * fix: Vue2ProvideUnheadPlugin is deprecated
    
    * feat: create UnheadPlugin for nuxt2
    
    * fix: use `setHeadInjectionHandler` to avoid head warnings
    
    * chore: remove replacement plugin
    
    ---------
    
    Co-authored-by: Harlan Wilton <harlan@harlanzw.com>

--- packages/bridge/src/runtime/head/plugins/unhead.ts
+++ packages/bridge/src/runtime/head/plugins/unhead.ts
@@ -1,7 +1,12 @@
-import { createHead as createClientHead, createServerHead, Vue2ProvideUnheadPlugin } from '@unhead/vue'
+import {
+  createHead as createClientHead,
+  createServerHead,
+  setHeadInjectionHandler,
+  Vue2ProvideUnheadPlugin
+} from '@unhead/vue'
 import { markRaw } from 'vue'
 import { renderSSRHead } from '@unhead/ssr'
-import { defineNuxtPlugin } from '../../app'
+import { defineNuxtPlugin, useNuxtApp } from '../../app'
 // @ts-ignore
 import metaConfig from '#build/meta.config.mjs'
 
@@ -9,9 +14,15 @@ export default defineNuxtPlugin((nuxtApp) => {
   const createHead = process.server ? createServerHead : createClientHead
   const head = createHead()
   head.push(markRaw(metaConfig.globalMeta))
-
+  // TODO the replacement plugin has issues in Nuxt, needs to be fixed upstream
   nuxtApp.vueApp.use(Vue2ProvideUnheadPlugin, head)
-  nuxtApp.vueApp.use(head)
+  nuxtApp.vueApp.config.globalProperties.$head = head
+
+  // allow useHead to be used outside a Vue context but within a Nuxt context
+  setHeadInjectionHandler(
+    // need a fresh instance of the nuxt app to avoid parallel requests interfering with each other
+    () => useNuxtApp().vueApp.config.globalProperties.$head
+  )
 
   if (process.server) {
       nuxtApp.ssrContext!.renderMeta = async () => {
