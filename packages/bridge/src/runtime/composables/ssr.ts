2ea2a8cd0c4618084ab1e69ee918c1dd9f92fa7d
Andrey Yolkin <andreyyolkin@gmail.com>
2023-12-13 16:39:40 +0300

    fix: polyfill event for `useRequestEvent` when nitro is disabled (#999)

--- packages/bridge/src/runtime/composables/ssr.ts
+++ packages/bridge/src/runtime/composables/ssr.ts
@@ -1,6 +1,6 @@
 import type { H3Event } from 'h3'
 import type { NuxtAppCompat } from '@nuxt/bridge-schema'
-import { getRequestHeaders } from 'h3'
+import { getRequestHeaders, createEvent } from 'h3'
 import { useNuxtApp } from '../nuxt'
 
 export function useRequestHeaders<K extends string = string> (include: K[]): { [key in Lowercase<K>]?: string }
@@ -14,5 +14,11 @@ export function useRequestHeaders (include?: any[]) {
 }
 
 export function useRequestEvent (nuxtApp: NuxtAppCompat = useNuxtApp()): H3Event {
-  return nuxtApp.ssrContext?.event as H3Event
+  // check if H3 event is available
+  if (nuxtApp.ssrContext?.event) { return nuxtApp.ssrContext.event }
+  // Check if we created H3 event manually before
+  if (nuxtApp.ssrContext?._event) { return nuxtApp.ssrContext._event }
+  // Create H3 event https://github.com/nuxt/bridge/pull/999#discussion_r1413049422
+  nuxtApp.ssrContext._event = createEvent(nuxtApp.ssrContext?.req, nuxtApp.ssrContext?.res)
+  return nuxtApp.ssrContext._event
 }
