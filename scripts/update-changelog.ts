a7edbffab6e82d74b6e187a3e39908033f1e991f
wattanx <wattan.dev@gmail.com>
2024-01-04 10:22:02 +0900

    ci: update changelog with new version

--- scripts/update-changelog.ts
+++ scripts/update-changelog.ts
@@ -15,7 +15,7 @@ async function main () {
   ))
   const bumpType = await determineBumpType()
 
-  const newVersion = inc(workspace.find('@nuxt/bridge').data.version, 'prerelease', 'rc')
+  const newVersion = inc(workspace.find('@nuxt/bridge').data.version, bumpType || 'patch')
   const changelog = await generateMarkDown(commits, config)
 
   // Create and push a branch with bumped versions if it has not already been created
