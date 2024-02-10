df50a7a9edd4f5fa58a6cb666f8a26d7bd09ca1b
wattanx <wattan.dev@gmail.com>
2024-02-10 14:37:07 +0900

    fix: change to internal

--- packages/bridge/src/runtime/composables/router.ts
+++ packages/bridge/src/runtime/composables/router.ts
@@ -52,7 +52,7 @@ function convertToLegacyMiddleware (middleware) {
       return ctx.error(result)
     }
     if (result) {
-      return ctx.redirect(ctx.ssrContext?.redirectCode || 302, result)
+      return ctx.redirect(ctx.ssrContext?._redirectCode || 302, result)
     }
     return result
   }
@@ -115,7 +115,7 @@ export const navigateTo = (to: RawLocation | undefined | null, options?: Navigat
 
       if (!isExternal && inMiddleware) {
         router.afterEach(final => final.fullPath === fullPath ? redirect(false) : undefined)
-        nuxtApp.ssrContext.redirectCode = options?.redirectCode || 302
+        nuxtApp.ssrContext._redirectCode = options?.redirectCode || 302
         return to
       }
 
