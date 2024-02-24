095ba36691d45f8cb57eebce68595d13b6abdd12
Daniel Roe <daniel@roe.dev>
2024-02-24 10:05:42 +0000

    test: always test tsx

--- test/bridge.test.ts
+++ test/bridge.test.ts
@@ -5,7 +5,6 @@ import { expectNoClientErrors, parseData, renderPage } from './utils'
 
 const isWebpack = process.env.TEST_BUILDER === 'webpack'
 const isDev = process.env.TEST_ENV === 'dev'
-const isTSX = process.env.TEST_TYPESCRIPT === 'isTSX' || process.env.TEST_TYPESCRIPT === 'esbuild'
 
 await setup({
   rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
@@ -113,7 +112,7 @@ describe('pages', () => {
   it('uses server Vue build', async () => {
     expect(await $fetch('/')).toContain('Rendered on server: true')
   })
-  it.skipIf(!isTSX)('supports jsx', async () => {
+  it('supports jsx', async () => {
     const html = await $fetch('/jsx')
 
     expect(html).toContain('JSX component')
