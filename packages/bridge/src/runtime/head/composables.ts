7731bdfa600b0389e5a39915a4e2fc924d2f39d9
Daniel Roe <daniel@roe.dev>
2025-01-06 15:31:45 +0000

    fix: pull `zhead` types into context

--- packages/bridge/src/runtime/head/composables.ts
+++ packages/bridge/src/runtime/head/composables.ts
@@ -1,6 +1,7 @@
 import { useHead } from '@unhead/vue'
 import type { ComputedGetter } from '@vue/reactivity'
 import type { MetaObject } from '@nuxt/schema'
+import type {} from 'zhead'
 
 // TODO: remove useMeta support when Nuxt 3 is stable
 /** @deprecated Please use new `useHead` composable instead */
