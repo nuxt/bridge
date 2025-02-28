134eb69a786da4d9df77a1fe82d831011ccf7226
Daniel Roe <daniel@roe.dev>
2025-02-28 13:51:43 +0000

    build: pull in types from `@unhead/schema`

--- packages/bridge/src/runtime/head/composables.ts
+++ packages/bridge/src/runtime/head/composables.ts
@@ -2,6 +2,7 @@ import { useHead } from '@unhead/vue'
 import type { ComputedGetter } from '@vue/reactivity'
 import type { MetaObject } from '@nuxt/schema'
 import type {} from 'zhead'
+import type {} from '@unhead/schema'
 
 // TODO: remove useMeta support when Nuxt 3 is stable
 /** @deprecated Please use new `useHead` composable instead */
