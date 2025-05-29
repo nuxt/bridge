3bace5088d6e4faf855a3459665e5c11da6c72ce
Daniel Roe <daniel@roe.dev>
2025-05-29 12:09:52 +0200

    fix: transpile `node-mock-http`

--- packages/bridge/src/app.ts
+++ packages/bridge/src/app.ts
@@ -44,7 +44,7 @@ export async function setupAppBridge (_options: any) {
   nuxt.options.build.transpile.push('vuex')
 
   // Transpile libs with modern syntax
-  nuxt.options.build.transpile.push('h3', 'iron-webcrypto', 'ohash', 'ofetch', 'unenv', 'cookie-es')
+  nuxt.options.build.transpile.push('h3', 'iron-webcrypto', 'ohash', 'ofetch', 'unenv', 'cookie-es', 'node-mock-http')
 
   // Transpile @unhead/vue and @unhead/ssr
   nuxt.options.build.transpile.push('unhead')
