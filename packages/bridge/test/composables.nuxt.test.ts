b2758fec6c1bd50e2ae05a5e07472339ad1232d6
wattanx <wattan.dev@gmail.com>
2025-06-04 23:46:47 +0900

    test: remove projects

--- packages/bridge/test/composables.nuxt.test.ts
+++ packages/bridge/test/composables.nuxt.test.ts
@@ -1,7 +1,17 @@
-import { describe, it, expect } from 'vitest'
-import { useCookie } from '#app'
+// @vitest-environment happy-dom
+
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { useCookie } from '../src/runtime'
 
 describe('useCookie', () => {
+  beforeAll(() => {
+    process.client = true
+  })
+
+  afterAll(() => {
+    process.client = false
+  })
+
   it('should set cookie value when called on client', () => {
     useCookie('cookie-watch-false', { default: () => 'foo', watch: false })
     expect(document.cookie).toContain('cookie-watch-false=foo')
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,28 +1,3 @@
 import { defineConfig } from 'vitest/config'
-import { resolve } from 'pathe'
 
-export default defineConfig({
-  test: {
-    projects: [
-      {
-        test: {
-          include: ['test/**/*.test.ts'],
-          exclude: ['test/nuxt/**/*.test.ts']
-        }
-      },
-      {
-        test: {
-
-          include: ['test/**/*.nuxt.test.ts'],
-          environment: 'happy-dom',
-          alias: {
-            '#app': resolve('./packages/bridge/src/runtime/index')
-          }
-        },
-        define: {
-          'process.client': 'true'
-        }
-      }
-    ]
-  }
-})
+export default defineConfig({})
